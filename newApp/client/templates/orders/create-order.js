936e3f2d4d2716b1d2e50fe5fb522afc3defc529
charue808 <charlesgum@gmail.com>
2016-11-01 08:46:21 -1000

    added in new comments create-order files

--- newApp/client/templates/orders/create-order.js
+++ newApp/client/templates/orders/create-order.js
@@ -1,23 +1,29 @@
-// Create an order
+// selected publisher name switches routes to edit order page
+// 
+
+
 
 Template.createOrder.events({
 // Selecting a publisher and then selecting next presents user with build order page
   // Next button event
   'click #next'(event) {
-    
+
+    // select function
+
+
     // Find the select element with value of publisher name
     let selected = $("#publisherList option:selected").val();
     console.log("the publisher you selected is: " , selected);
-    
+
     // Create Order Collection Document with publisher name, created date and order number
-   
-    
+
+
     // Route to build order template with parameter of publisher and order status
   }
 });
 
 
-        
+
         // Look for Order Collection documents with publisher name equal to selected element value and order status of current order
 
         // if order collection document found has publisher name that is true and order status that is current order than return Order document

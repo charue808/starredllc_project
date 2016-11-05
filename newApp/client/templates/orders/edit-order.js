55a42332938f5da9250bc01472b02d7b558a56a9
charue808 <charlesgum@gmail.com>
2016-11-05 11:15:48 -1000

    clean up sortItems feature branch to prepare for merge to master

--- newApp/client/templates/orders/edit-order.js
+++ newApp/client/templates/orders/edit-order.js
@@ -3,11 +3,11 @@
 Template.editOrderContextSet.onRendered(function() {
 
   // Load session variable with (sample) order from Order collection
-  //
+  // Define publisherId from URL
   let publisherId = FlowRouter.getQueryParam("publisherId");
-
+  // Create session variable
   Session.set('setOrder', {"publisherName":publisherId, "_id":"", "status": "pending"});
-  console.log("This is my  order detail: ", Session.get('setOrder'));
+  // console.log("This is my order detail: ", Session.get('setOrder'));
 });
 
 Template.editOrderContextSet.helpers({
@@ -15,4 +15,3 @@ Template.editOrderContextSet.helpers({
    return Session.get('setOrder');
   }
 });
-
